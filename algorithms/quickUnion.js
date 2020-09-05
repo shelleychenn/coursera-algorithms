b4c77ef1ad425a0149132690594e9edd03d42148
shelleychenn <shelleychen322@gmail.com>
2020-09-05 13:46:38 -0400

    Comment on time complexity for quick union

--- algorithms/quickUnion.js
+++ algorithms/quickUnion.js
@@ -1,6 +1,8 @@
 class QuickUnion {
   constructor(n) {
     this.data = [];
+
+    //set id of each object to itself
     for (let i = 0; i < n; i++) {
       this.data[i] = i;
     }
@@ -9,6 +11,7 @@ class QuickUnion {
   root(n) {
     let root = this.data[n];
 
+    //chase parent pointers until reach root
     while (this.data[root] !== root) {
       root = this.data[root];
     }
@@ -18,11 +21,24 @@ class QuickUnion {
   union(p, q) {
     const rootp = this.root(p);
     const rootq = this.root(q);
-
+    //change root of p to point to root of q
     this.data[rootp] = rootq;
   }
 
   isConnected(p, q) {
+    //check if p and q have the same root
     return this.root(p) === this.root(q);
   }
 }
+
+/*
+
+algorithm: quick-union
+
+initialize: O(N)
+union: O(N)
+find(isConnected): O(N)
+
+defect: faster then quickfind but also too slow; the trees can get too tall; find too expensive
+
+*/
